2612b467b43e8bdf2a2290c8e669ab1b9d347323
Sadia-js <sjsthi610@gmail.com>
2024-07-02 03:40:38 +0100

    my corrected website

--- script.js
+++ script.js
@@ -35,24 +35,7 @@ toogleBtn.addEventListener('click', () =>{
     }
 });
 
-// let modebtn = document.querySelector('#mode');
-// let body =  document.querySelector('body');
-// let currMode = 'light'; //dark
 
-// modebtn.addEventListener('click', ()=>{
-//     if(currMode === 'light'){
-//         currMode = 'dark';
-//         body.classList.add('dark');
-//         body.classList.remove('light');
-      
-//     }
-// else{
-//     currMode = 'light';
-//     body.classList.add('light');
-//     body.classList.remove('dark');
-// }
-// console.log(currMode);
-// });
 
 
 
